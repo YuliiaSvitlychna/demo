56d34aa21930464ae0f16eea9cc25ae784fad23f
Yuliia Svitlychna <yuliia.svitlychna@gmail.com>
2025-01-23 00:37:15 +0200

    chore: disable swagger for prod

--- nestjs-typeorm/src/main.ts
+++ nestjs-typeorm/src/main.ts
@@ -2,22 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  const config = new DocumentBuilder()
-    .setTitle('API Documentation')
-    .setDescription('The API description')
-    .setVersion('1.0')
-    // .addBearerAuth()
-    .build();
+  if (configService.get<string>('NODE_ENV') !== 'prod') {
+    const config = new DocumentBuilder()
+      .setTitle('API Documentation')
+      .setDescription('The API description')
+      .setVersion('1.0')
+      // .addBearerAuth()
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(configService.get<number>('NODE_PORT') ?? 3000);
 }
 bootstrap();
